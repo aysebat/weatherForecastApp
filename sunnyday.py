39c4b0b8e37196011569be3a48592fd8a7467cff
aysebat <aaysebatt@gmail.com>
2023-03-09 11:38:25 +0300

    add the lat and lon argument to get lattitude and longitute from the api

--- sunnyday.py
+++ sunnyday.py
@@ -4,14 +4,25 @@ apikey="4429faaaa08bd6135bd6b3c567c46811"
 
 class Weather:
 
-
-
-    def __init__(self, apikey, city, lat=None, lon=None):
-        url = f"https: // api.openweathermap.org / data / 2.5 / weather?q = {city} & appid = {apikey}"
+    def __init__(self, apikey=None, city=None, lat=None, lon=None):
         self.apikey = apikey
         self.city = city
         self.lat = lat
         self.lon = lon
+        if apikey:
+            if city:
+                url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
+            elif lat and lon:
+                url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}"
+            else:
+                print("Provide either a city or lat and lon argument.")
+        else:
+            print("Please! Provide a valid API key.")
+        r = requests.get(url)
+        self.data = r.json()
+
+
+
 
     def next_12h(self):
         pass
@@ -20,3 +31,6 @@ class Weather:
         pass
 
 
+#weather = Weather(apikey=apikey, city="Roma")
+weather = Weather(apikey=apikey, lat=4.1, lon=3.2)
+print(weather.data)
