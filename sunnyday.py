2158d7c9a747b9f77c11632ec670fcf98658871a
aysebat <aaysebatt@gmail.com>
2023-03-10 13:36:30 +0300

    add the dotstring

--- sunnyday.py
+++ sunnyday.py
@@ -4,6 +4,24 @@ from datetime import datetime
 apikey="4429faaaa08bd6135bd6b3c567c46811"
 
 class Weather:
+    """"
+    Creates a Weather object getting apikey as input
+    and either a city name or lat and lot coordinates.
+
+    Package use example:
+    # Create weather object using a city name:
+    # The api key below is not guranteed to work.
+    # Get your own api key from https://openweathermap.org
+    #And wait a couple of hour the apikey to be activated
+    >> apikey="4429faaaa08bd6135bd6b3c567c46811"
+    >> weather1 = Weather(apikey=apikey, city="Roma")
+
+    >>##Using lattitude and longitude
+    >>weather2 = Weather(apikey=apikey, lat=4.1, lon=3.2)
+
+    >>##get coplete for the weather data
+    >>weather1.weather_data()
+    """
 
     def __init__(self, apikey=None, city=None, lat=None, lon=None):
         self.apikey = apikey
@@ -22,6 +40,10 @@ class Weather:
 
         r = requests.get(url)
         self.data = r.json()
+        #if the user not able connect the API raise and error!
+        if self.data["cod"] != "200":
+            raise ValueError(self.data["message"])
+
 
 
 
@@ -34,6 +56,7 @@ class Weather:
 
 
 
-#weather = Weather(apikey=apikey, city="Roma")
-weather = Weather(apikey=apikey, lat=4.1, lon=3.2)
+
+weather = Weather(apikey=apikey, city="Roma")
+#weather = Weather(apikey=apikey, lat=4.1, lon=3.2)
 pprint.pprint(weather.weather_data())
