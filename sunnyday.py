d8c4ccea64bf15cb92929ebb33df6d89e38d23ee
aysebat <aaysebatt@gmail.com>
2023-03-10 13:50:17 +0300

    fix on the syntex

--- sunnyday.py
+++ sunnyday.py
@@ -1,8 +1,7 @@
-import requests, pprint
+import requests
 from datetime import datetime
 
-apikey="4429faaaa08bd6135bd6b3c567c46811"
-
+apikey = "4429faaaa08bd6135bd6b3c567c46811"
 class Weather:
     """"
     Creates a Weather object getting apikey as input
@@ -13,13 +12,14 @@ class Weather:
     # The api key below is not guranteed to work.
     # Get your own api key from https://openweathermap.org
     #And wait a couple of hour the apikey to be activated
+
     >> apikey="4429faaaa08bd6135bd6b3c567c46811"
     >> weather1 = Weather(apikey=apikey, city="Roma")
 
-    >>##Using lattitude and longitude
+    ##Using lattitude and longitude
     >>weather2 = Weather(apikey=apikey, lat=4.1, lon=3.2)
 
-    >>##get coplete for the weather data
+    ##get coplete for the weather data
     >>weather1.weather_data()
     """
 
@@ -28,35 +28,23 @@ class Weather:
         self.city = city
         self.lat = lat
         self.lon = lon
-        if apikey:
-            if city:
-                url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
-            elif lat and lon:
-                url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}"
+        try:
+            if apikey:
+                if city:
+                    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
+                elif lat and lon:
+                    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}"
+                else:
+                    raise TypeError("Provide either a city or lat and lon argument.")
             else:
-                raise TypeError("Provide either a city or lat and lon argument.")
-        else:
-            raise TypeError("Please! Provide a valid API key.")
+                raise TypeError("Please! Provide a valid API key.")
 
-        r = requests.get(url)
-        self.data = r.json()
-        #if the user not able connect the API raise and error!
-        if self.data["cod"] != "200":
+            r = requests.get(url)
+            self.data = r.json()
+        except:
             raise ValueError(self.data["message"])
 
-
-
-
-
     def weather_data(self):
-
-        #return (self.data)
-        return (datetime.fromtimestamp(self.data['dt']).isoformat(), self.data['main']['temp'], self.data['weather'][0]['description'])
-
-
-
-
-
-weather = Weather(apikey=apikey, city="Roma")
-#weather = Weather(apikey=apikey, lat=4.1, lon=3.2)
-pprint.pprint(weather.weather_data())
+        return (datetime.fromtimestamp(self.data['dt']).isoformat(),
+                self.data['main']['temp'],
+                self.data['weather'][0]['description'])
